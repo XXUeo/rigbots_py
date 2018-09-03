169f2a6edd2445dcb40ea0446db57e1fc5bb1306
NandN <nobi3892@gmail.com>
2018-09-03 12:53:19 +0900

    simple parser for yml

--- rigbots/config.py
+++ rigbots/config.py
@@ -1,1 +1,37 @@
-aa
+#!/usr/bin/env python
+
+
+import yaml
+
+from log_helper import get_logger
+
+
+l = get_logger('rigbots')
+
+#this class is the parser for yml file
+class Config:
+
+	def parser():
+
+		filename = "../settings.yml"
+
+		with open(filename, 'r') as stream:
+			try:
+				l.info('Parsing Config Files')
+				l.info('Opening {}'.format(filename))
+				l.info('{} Successfully Read'.format(filename))
+				val = yaml.load(stream)
+			#	print(val)
+			except yaml.YAMLError as exc:
+				l.fatal(
+                    'Error parsing file {filename}: {exc}. Please fix and try '
+                    'again.'.format(filename=filename, exc=exc))
+			#	print(exc)
+
+		return val
+
+
+
+	if __name__ == "__main__":
+		print (parser())
+		
--- rigbots/ethosapi.py
+++ rigbots/ethosapi.py
@@ -1,1 +1,16 @@
-aa
+#!/usr/bin/env python
+
+from pyEthOS import pyEthOS as ethos
+import requests
+import json
+
+
+
+class EthosApi:
+
+	def __init__():
+
+
+
+	../
+
