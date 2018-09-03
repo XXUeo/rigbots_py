d9e82852b6150570fe346524c92eebc0209b1265
NandN <nobi3892@gmail.com>
2018-09-03 14:06:05 +0900

    basic api call donw

--- rigbots/config.py
+++ rigbots/config.py
@@ -6,32 +6,40 @@ import yaml
 from log_helper import get_logger
 
 
-l = get_logger('rigbots')
+l = get_logger('config')
 
 #this class is the parser for yml file
-class Config:
-
-	def parser():
-
-		filename = "../settings.yml"
-
-		with open(filename, 'r') as stream:
-			try:
-				l.info('Parsing Config Files')
-				l.info('Opening {}'.format(filename))
-				l.info('{} Successfully Read'.format(filename))
-				val = yaml.load(stream)
-			#	print(val)
-			except yaml.YAMLError as exc:
-				l.fatal(
-                    'Error parsing file {filename}: {exc}. Please fix and try '
-                    'again.'.format(filename=filename, exc=exc))
-			#	print(exc)
-
-		return val
 
 
+def parser():
 
-	if __name__ == "__main__":
-		print (parser())
-		
+	filename = "../settings.yml"
+
+	with open(filename, 'r') as stream:
+		try:
+			l.info('Parsing Config Files')
+			l.info('Opening {}'.format(filename))
+			l.info('{} Successfully Read'.format(filename))
+			val = yaml.load(stream)
+		#	print(val)
+		except yaml.YAMLError as exc:
+			l.fatal(
+                'Error parsing file {filename}: {exc}. Please fix and try '
+                'again.'.format(filename=filename, exc=exc))
+		#	print(exc)
+
+	return val
+
+def ethosUrl():
+	s = parser()
+	sa = s['ethosLink']
+	
+	l.info('ethOS panel name = ' + sa )
+	return str(sa)
+
+
+if __name__ == "__main__":
+	#print (parser())
+	s = parser()
+
+	
--- rigbots/ethosapi.py
+++ rigbots/ethosapi.py
@@ -4,13 +4,41 @@ from pyEthOS import pyEthOS as ethos
 import requests
 import json
 
+from config import ethosUrl
+from log_helper import get_logger
 
 
-class EthosApi:
+l = get_logger('ethosapi')
 
-	def __init__():
+def ethosapi():
+	
+	PANEL_NAME = ethosUrl()
+	DEBUG = False
+
+	#api = ethos.EthOS_API(PANEL_NAME, debug=DEBUG)
+
+
+	try:
+		api = ethos.EthOS_API(PANEL_NAME, debug=DEBUG)
+	except requests.exceptions.RequestException as e:
+		print(e)
+		sys.exit(1)
+
+
+	#req = requests.get('http://altaiu.ethosdistro.com')
+	#req2 = req.json()
+	apiReq = api.get_summary()
+
+	dump = json.dumps(apiReq)
+	data = json.loads(dump)
 
 
 
-	../
+
+
+if __name__ == "__main__":
+
+	ethosapi()
+
+
 
